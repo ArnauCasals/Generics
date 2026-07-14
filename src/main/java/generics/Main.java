56c33c5e4627154edab2a5b19ea833851301429b
Arnau <arnau@MacBook-de-Arnau.local>
2026-07-14 12:38:05 +0200

    Complete exercise 1

--- src/main/java/generics/Main.java
+++ src/main/java/generics/Main.java
@@ -2,7 +2,13 @@ package generics;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+        NoGenericMethods object =
+                new NoGenericMethods("Tres", "Uno", "Dos");
+
+        System.out.println(object.getElement1());
+        System.out.println(object.getElement2());
+        System.out.println(object.getElement3());
     }
 }
